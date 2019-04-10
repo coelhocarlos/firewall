c62346e6dcc1ee5fcd68a593d201baa03047790d
carlos coelho <carloscoelho_@msn.com>
2019-04-09 22:06:57 -0300

    Update firewall_2019.sh

--- firewall_2019.sh
+++ firewall_2019.sh
@@ -1,202 +1,332 @@
-#!/bin/bash
+#! /bin/bash 
+#Configuracao Firewall atraves do iptables
+#Autoria do Script
+#"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+#"| Script de Firewall - IPTABLES"
+#"| Criado por: "
+#"| membros da comunidade viva o linux"
+#"| Tecnico em Informatica"
+#"| ""
+#"| Uso: firewall start|stop|restart"
+#"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 
-### BEGIN INIT INFO
-# Provides:          firewall.sh
-# Required-Start:    $local_fs $remote_fs $network $syslog
-# Required-Stop:     $local_fs $remote_fs $network $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start firewall.sh at boot time
-# Description:       Enable service provided by firewall.sh.
-### END INIT INFO
+###########################
+#######TITULO ABRE####### #
+echo "Iniciando FIREWALL"
+###########################
 
-iniciar_firewall(){
-echo "################################################"
-echo "# SCRIPT FIREWALL - COMPARTILHAMENTO DE REDE   #"
-echo "# TÉCNICO                                      #"
-echo "# 08/05/2019                                   #"
-echo "################################################"
+##################################################################
+#Os diversos modulos do iptables sao chamados atraves do modprobe#
+##################################################################
 
-externa="enp2s0"
-interna="enp2s0"
-rede_interna="192.168.0.0/24"
-
-echo "Interface Rede Externa .......................... "$externa
-echo "Interface Rede Interna .......................... "$interna
-echo "Faixa de IP Rede Interna ........................ "$rede_interna
-
-echo "Limpando Politicas .............................. [ OK ]" 
-iptables -X
-iptables -Z
-iptables -F INPUT
-iptables -F OUTPUT
-iptables -F FORWARD
-iptables -F -t nat
-iptables -F -t filter
-iptables -F -t mangle
-
-echo "Aplicando nova politica ......................... [ OK ]"
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
-
-echo "Ativa Modulos Basicos ........................... [ OK ]"
 modprobe ip_tables
-modprobe ip_conntrack
-modprobe iptable_filter
-modprobe iptable_mangle
 modprobe iptable_nat
-modprobe ipt_LOG
-modprobe ipt_limit
-modprobe ipt_state
-modprobe ipt_REDIRECT
-modprobe ipt_owner
-modprobe ipt_REJECT
-modprobe ipt_MASQUERADE
+modprobe ip_conntrack
 modprobe ip_conntrack_ftp
 modprobe ip_nat_ftp
+modprobe ipt_LOG
+modprobe ipt_REJECT
+modprobe ipt_MASQUERADE
+modprobe ipt_state
+modprobe ipt_multiport
+modprobe iptable_mangle
+modprobe ipt_tos
+modprobe ipt_limit
+modprobe ipt_mark
+modprobe ipt_MARK
 
-echo "Roteamento de Kernal ............................ [ OK ]"
-echo 1 > /proc/sys/net/ipv4/ip_forward
+echo "subindo os modulos do IPTABLE e NETFILTRE"
+echo "ON .......................................................[#OK]"
 
-echo "Compartilhar Internet ........................... [ OK ]"
-iptables -t nat -A POSTROUTING -o $externa -j MASQUERADE
+####################
+#Interfaces de Rede#
+####################
+LAN=enp2s0
 
-echo "Mantendo conexões estabelecidas ................. [ OK ]"
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -m state --state ESTABLISHED,RELATED,NEW -j ACCEPT
+echo "ativado o placa de rede enp2s0"
+echo "ON ........................................................[#OK]"
+
+###########################
+#Mensagem de inicializacao#
+###########################
+
+echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "| Script de Firewall - IPTABLES"
+echo "| Criado por: Josue Santos"
+echo "| membro da comunidade viva o linux"
+echo "| Tecnico em Informatica"
+echo "| josueandres.fcb@gmail.com"
+echo "| Uso: firewall start|stop|restart"
+echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo 
+echo "=========================================================|"
+echo "|:INICIANDO A CONFIGURACAO DO FIREWALL NETFILTER ATRAVES:|"
+echo "|:                    DO IPTABLES                       :|"
+echo "=========================================================|"
+
+######################
+#Zera todas as Regras#
+######################
+
+echo "Zerando todas as Regras"
+iptables -F
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+
+########################################
+##Bloqueia tudo, nada entra e nada sai##
+########################################
+
+echo "Fechando tudo..."
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+
+###################################
+###Libera conexoes estabelecidas###
+###################################
+
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -m state --state RELATED,ESTABLISHED,NEW -j ACCEPT
 iptables -A OUTPUT -m state --state RELATED,ESTABLISHED,NEW -j ACCEPT
 
-echo "Logs de bloqueios ............................... [ OK ]"
-iptables -A INPUT -j LOG
-iptables -A OUTPUT -j LOG
-iptables -A FORWARD -j LOG
+iptables -A INPUT -i lo -j ACCEPT
 
-echo "Regras DNAT para servidor WEB ................... [ OK ]"
-iptables -t nat -A PREROUTING -i $externa -p tcp --dport 80 -j DNAT --to 192.168.0.50:8082
+echo "Liberando conexoes..."
+echo "ON.......................................................[#ON]"
 
-echo "Bloqueio de sites ............................... [ OK ]"
-iptables -A FORWARD -d 173.252.91.4/25 -j REJECT
-iptables -A FORWARD -d 31.13.73.1/25 -j REJECT
+###################
+#COMPARTILHAMENTO##
+###################
 
-#REGRAS FORWARD
-echo "Permite ping pra Rede Externa ................... [ OK ]"
-iptables -A FORWARD -i $interna -o $externa -p icmp -j ACCEPT
+iptables -t nat -A POSTROUTING -o $LAN -j MASQUERADE
 
-echo "Permite conexões da Rede Interna pra Externa .... [ OK ]"
-iptables -A FORWARD -i $interna -o $externa -p tcp -m multiport --dports 80,443,3128,110,20,21,587,995,143,22,3389,25,5900,5100,3389,9966,10000,11000,32400 -j ACCEPT
+echo "Libera Compartilhamento"
+echo "ON.......................................................[#ON]"
 
-echo "Regras cliente .................................. [ OK ]"
-iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
-iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+############################################
+#Bloqueio de scanners ocultos (Shealt Scan)#
+############################################
 
-echo "Regras Redirecionando ............................[ OK ]"
-echo "1" > /proc/sys/net/ipv4/ip_forward 
+iptables -A FORWARD -p tcp --tcp-flags SYN,ACK, FIN,  -m limit --limit 1/s -j ACCEPT
+echo "bloqueado scanners ocultos"
+echo "ON ......................................................[#OK]"
+
+#########################
+#Bloqueio Anti-Spoofings#
+#########################
+
+iptables -A INPUT -s 127.0.0.0/8 -i $LAN -j DROP
+iptables -A INPUT -s 127.0.0.0/8 -i $LAN  -j DROP
+iptables -A INPUT -s 192.168.0.0/24 -i $LAN  -j DROP
+iptables -A INPUT -s 192.168.0.0/24 -i $LAN  -j DROP
+
+echo "ativado o bloqueio de tentativa de ataque do tipo Anti-spoofings"
+echo "ON .....................................................[#OK]"
+
+#######################################
+#Bloqueio de ataque ssh de forca bruta#
+#######################################
+
+iptables -N SSH-BRUT-FORCE
+iptables -A INPUT -i $LAN -p tcp --dport 22 -j SSH-BRUT-FORCE
+iptables -A SSH-BRUT-FORCE -m limit --limit 1/s --limit-burst 4 -j RETURN
+iptables -A SSH-BRUT-FORCE -j DROP
+
+echo "ativado o bloqueio a tentativa de ataque do tipo SSH-BRUT-FORCE"
+echo "ON .....................................................[#OK]"
+
+###################################
+#Bloquear ataque do tipo SYN-FLOOD#
+###################################
+
+echo "0" > /proc/sys/net/ipv4/tcp_syncookies
+iptables -N syn-flood
+iptables -A INPUT -i $LAN -p tcp --syn -j syn-flood
+iptables -A syn-flood -m limit --limit 1/s --limit-burst 4 -j RETURN
+iptables -A syn-flood -j DROP
+
+echo "ativado o bloqueio de ataque do tipo SYN-FLOOD"
+echo "ON .....................................................[#OK]"
+
+################################################# 
+#Descarte de pacotes nao-identificado ICMP (ping)
+#################################################
+
+iptables -A OUTPUT -m state -p icmp --state INVALID -j DROP
+
+echo "ativado o bloqueio a tentativa de ataque do tipo PING-ICMP"
+echo "ON .....................................................[#OK]"
+
+######################
+#Contra Pings da morte
+######################
+
+echo "0" > /proc/sys/net/ipv4/icmp_echo_ignore_all
+iptables -N PING-MORTE
+iptables -A INPUT -p icmp --icmp-type echo-request -j PING-MORTE
+iptables -A PING-MORTE -m limit --limit 1/s --limit-burst 4 -j RETURN
+iptables -A PING-MORTE -j DROP
+
+echo "ativado o bloqueio a ataque do tipo ping da morte"
+echo "ON .....................................................[#OK]"
+
+############################################################################
+##Impede ataques DoS a maquina limitando a quantidade de respostas do ping##
+############################################################################
+
+iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j DROP
+
+echo "Previne ataques DoS"
+echo "ON .....................................................[#OK]"
+
+#################################
+##Bloqueia completamente o ping##
+#################################
+
+iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
+
+echo "Previne ataques PING"
+echo "ON .....................................................[#OK]"
+
+##########################
+##Politicas de seguranca##
+##########################
+
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects 
+echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+echo 1 > /proc/sys/net/ipv4/tcp_syncookies
+echo 1 > /proc/sys/net/ipv4/conf/default/rp_filter
+iptables -A INPUT -m state --state INVALID -j DROP
+
+echo "Implementacao de politicas de seguranca"
+echo "ON .....................................................[#OK]"
+
+#######################################################################################
+##Libera o acesso via SSH e Limita o numero de tentativas de acesso a 4 a cada minuto##
+#######################################################################################
+
+iptables -I INPUT -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --update --seconds 60 --hitcount 4 -j DROP
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p udp --dport 22 -j ACCEPT
+
+# [ SSH Interno ]
+iptables -t nat -A PREROUTING -p tcp --dport 22 -s 192.168.0.50/24 -j ACCEPT
+iptables -t nat -A PREROUTING -p udp --dport 22 -s 192.168.0.50/24 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -s 192.168.0.50/24 -j ACCEPT
+iptables -A INPUT -p udp --dport 22 -s 192.168.0.50/24 -j ACCEPT
+
+# [ SSH Externo ]
+iptables -t nat -A PREROUTING -p tcp --dport 22 -j ACCEPT
+iptables -t nat -A PREROUTING -p udp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p udp --dport 22 -j ACCEPT
+
+echo "Liberando SSH"
+echo "On......................................................[#OK]"
+#####################
+##Libera as Cameras##
+#####################
 iptables -t nat -A PREROUTING -i enp2s0 -p tcp --dport 42474 -m conntrack --ctstate NEW -j DNAT --to 192.168.0.60:42474
 iptables -t nat -A PREROUTING -i enp2s0 -p tcp --dport 9966 -m conntrack --ctstate NEW -j DNAT --to 192.168.0.60:9966
 iptables -t nat -A PREROUTING -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A POSTROUTING -t nat -j MASQUERADE
+echo "Liberando Cameras em rede Roteadas"
+echo "On......................................................[#OK]"
 
-echo "Regras servidor ................................. [ OK ]"
-iptables -A INPUT -p tcp -i $interna --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp -i $interna -m multiport --dports 3128,443,445,113,80,587,25,22,110,53,139,5900,5100,3389,9966,32400 -j ACCEPT
-iptables -A INPUT -p udp -i $interna -m multiport --dports 53,110,67,68,137,113,443,138 -j ACCEPT 
+#################
+##Libera Webmin##
+#################
+iptables -t nat -A PREROUTING -p tcp --dport 10000 -j ACCEPT
+iptables -t nat -A PREROUTING -p udp --dport 10000 -j ACCEPT
+iptables -A INPUT -p tcp --dport 10000 -j ACCEPT
+iptables -A INPUT -p udp --dport 10000 -j ACCEPT
 
-#echo "Redirecionamento de porta ....................... [ OK ]"
-#iptables -t nat -A PREROUTING -i $interna -p tcp --dport 80 -j REDIRECT --to-port 3128 
+echo "Liberando Webmin porta padrão 10000"
+echo "On......................................................[#OK]"
+#alternative Port Webmin
+iptables -t nat -A PREROUTING -p tcp --dport 10000 -j ACCEPT
+iptables -t nat -A PREROUTING -p udp --dport 10000 -j ACCEPT
+iptables -A INPUT -p tcp --dport 10000 -j ACCEPT
+iptables -A INPUT -p udp --dport 10000 -j ACCEPT
 
-echo "Liberando ping ICMP ............................. [ OK ]"
-iptables -A INPUT -p icmp -m limit --limit 1/s --limit-burst 1 -j ACCEPT
-iptables -A INPUT -p icmp -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix PING-DROP:
-iptables -A INPUT -p icmp -j DROP
-iptables -A OUTPUT -p icmp -j ACCEPT
+echo "Liberando Webmin porta alternativa 11000"
+echo "On......................................................[#OK]"
 
-echo "Permite conexões loopback ....................... [ OK ]"
+##################
+##Libera o Samba##
+##################
+
+iptables -A INPUT -p tcp --dport 137:139 -j ACCEPT
+iptables -A INPUT -p udp --dport 137:139 -j ACCEPT
+iptables -A INPUT -p tcp --dport 445 -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -p udp --dport 137 -j ACCEPT
+iptables -A INPUT -p udp --dport 138 -j ACCEPT
+iptables -A INPUT -p tcp --dport 139 -j ACCEPT
+iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachabl
 
-echo "##################### SEGURANÇA ########################"
+echo "Liberando Samba Compartilhamento do Samba"
+echo "On......................................................[#OK]"
 
-echo "Segurança contra IP Spoofing .................... [ OK ]"
-echo 1 > /proc/sys/net/ipv4/conf/default/rp_filter
-iptables -A INPUT -m state --state INVALID -j DROP
+###################
+##Libera o Apache##
+###################
 
-echo "Proteção contra alteração de rota ............... [ OK ]"
-echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
+echo "Liberando o Apache"
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8081 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8082 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8083 -j ACCEPT
 
-echo "Proteção contra alteração de caminho ............ [ OK ]"
-echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route
 
-echo "Proteção contra responses bogus ................. [ OK ]"
-echo 1 > /proc/sys/net/ipv4/icmp_ignore_bogus_error_responses
+####################### 
+#Bloquear Back Orifice#
+#######################
 
-echo "Bloqueando traceroute ........................... [ OK ]"
-iptables -A INPUT -p udp -s 0/0 -i $interna --dport 33435:33525 -j DROP
+iptables -A INPUT -p tcp --dport 31337 -j DROP
+iptables -A INPUT -p udp --dport 31337 -j DROP
+ 
+echo "ativado o bloqueio a tentativa de ataque do tipo BACK-ORIFICE"
+echo "ON .....................................................[#OK]"
 
-echo "Proteção para SYN flood ......................... [ OK ]"
-echo 1 > /proc/sys/net/ipv4/tcp_syncookies
-iptables -A FORWARD -p tcp --syn -m limit --limit 1/s -j ACCEPT
-iptables -A FORWARD -p tcp --syn -j DROP
+#################
+#Bloquear NetBus#
+#################
 
-echo "Negando portas invalidas (trojans, trinoo) ...... [ OK ]"
-iptables -A INPUT -p tcp -i $externa -m multiport --dports 666,4000,6000,6006,16660,27444,27665,31335,34555,35555 -j DROP
-iptables -A INPUT -p tcp -i $interna -m multiport --dports 1433,6670,6711,6712,6713,12345,12346,20034,31337,6000 -j DROP
+iptables -A INPUT -p tcp --dport 12345:12346 -j DROP
+iptables -A INPUT -p udp --dport 12345:12346 -j DROP
 
-echo "Proteção contra telnet .......................... [ OK ]"
-iptables -A INPUT -p TCP -i $externa --dport telnet -j DROP
+echo "ativado o bloqueio a tentativa de ataque do tipo NET-BUS"
+echo "ON .....................................................[#OK]"
+##########################
+#S#alvando Configurações##
+##########################
+iptables-save > /etc/webmin/firewall/iptables.save
+iptables-restore < /etc/webmin/firewall/iptables.save
 
-echo "Dropando pacotes TCP indesejaveis ............... [ OK ]"
-iptables -A FORWARD -p tcp ! --syn -m state --state NEW -j DROP
+echo "Salvando todas as Configurações"
+echo "ON .....................................................[#OK]"
+################
+##TITULO FECHA##
+################
 
-echo "Proteção contra worms ........................... [ OK ]"
-iptables -A FORWARD -p tcp --dport 135 -i $externa -j REJECT
-
-echo "Proteção contra ICMP Broadcasting ............... [ OK ]"
-echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
-
-echo "Proteção contra Port Scanners ocultos ........... [ OK ]"
-iptables -A INPUT -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j ACCEPT
-iptables -A FORWARD -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j ACCEPT
-
-echo "Proteção contra Ping da morte ................... [ OK ]"
-iptables -A FORWARD -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
-
-echo "Proteção contra IP Spoofing ..................... [ OK ]"
-iptables -A INPUT -s 172.16.0.0/12 -i $externa -j DROP
-iptables -A INPUT -s 127.0.0.0/8 -i $externa -j DROP
-iptables -A INPUT -s 10.0.0.0/8 -i $externa -j DROP
-iptables -A INPUT -s 192.168.0.0/16 -i $externa -j DROP
-
-echo "Bloqueando Pacotes Fragmentados.................. [ OK ]"
-iptables -A INPUT -i $externa -f -j LOG --log-prefix "Pacote Fragmentado: "
-iptables -A INPUT -i $externa -f -j DROP
-iptables -A INPUT -i $interna -f -j LOG --log-prefix "Pacote Fragmentado: "
-iptables -A INPUT -i $interna -f -j DROP
-
-}
-parar_firewall(){
-
-echo "Parando Firewall ................................ [ OK ]"
-iptables -X
-iptables -F 
-iptables -t nat -F
-iptables -t filter -F
-iptables -t mangle -F
-
-echo "Liberando Politicas ............................. [ OK ]"
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
-
-echo "Parando  ................................ [ OK ]"
-echo 0 > /proc/sys/net/ipv4/ip_forward
-
-echo "Firewall Desativado............................. [ OK ]"
-}
-
-case "$1" in
-"start") iniciar_firewall ;;
-"stop") parar_firewall ;;
-"restart") parar_firewall; iniciar_firewall;;
-*) echo "Use os parametros | start | stop | restart |"
-esac
+echo "Configuracao Firewall Concluida."
+echo
+echo "==========================================================|"
+echo "::TERMINADA A CONFIGURACAO FIREWALL NETFILTER ATRAVES   ::|"
+echo "::                  DO IPTABLES                         ::|"
+echo "==========================================================|"
+echo "FIREWALL ATIVADO - SISTEMA PREPARADO"
+echo "SCRIPT DE FIREWALL CRIADO POR :-)  :-)"
+echo "FIREWALL DESCARREGADO - SISTEMA LIBERADO"
